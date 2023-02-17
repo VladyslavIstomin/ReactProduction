9b89875ac7ac4b3e4f06232a3cc849bb847aa96d
Vladyslav_Istomin <vladyslav_istomin@epam.com>
2023-02-17 19:18:17 +0200

    added visual report

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,16 +1,16 @@
 import { screen } from '@testing-library/react';
 import { Sidebar } from 'widgets/Sidebar';
-import { RenderWithTranslation } from 'shared/lib/tests/RenderWithTranslation';
 import userEvent from '@testing-library/user-event';
+import { ComponentRender } from 'shared/lib/tests/ComponentRender';
 
 describe('Sidebar', () => {
     test('with only first param', () => {
-        RenderWithTranslation(<Sidebar />);
+        ComponentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     test('sidebar toggle', () => {
-        RenderWithTranslation(<Sidebar />);
+        ComponentRender(<Sidebar />);
         const btn = screen.getByTestId('sidebar-btn');
         userEvent.click(btn);
         expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
