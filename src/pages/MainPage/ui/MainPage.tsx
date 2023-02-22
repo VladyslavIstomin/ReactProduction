d1fe827e5eabe5cf0b4ae73d6ce75caa32aae7e5
Vladyslav_Istomin <vladyslav_istomin@epam.com>
2023-02-22 12:42:36 +0200

    removed Counter component

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,5 +1,4 @@
 import { useTranslation } from 'react-i18next';
-import { Counter } from 'entities/Counter';
 
 const MainPage = () => {
     const { t } = useTranslation();
@@ -7,7 +6,6 @@ const MainPage = () => {
     return (
         <div>
             <h1>{t('Main page')}</h1>
-            <Counter/>
         </div>
     );
 };
