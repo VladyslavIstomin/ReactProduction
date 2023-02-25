ebb9f540a24f11b9bb2a83227342dbff0ab4a1a3
Vladyslav_Istomin <vladyslav_istomin@epam.com>
2023-02-25 17:51:12 +0200

    updated Navbar ui screenshots

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -18,12 +18,14 @@ const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
 
 export const Light = Template.bind({});
 Light.args = {};
-Light.decorators = [StoreDecorator({
-    login: { username: 'efwef', password: '123' }
-})];
+Light.decorators = [StoreDecorator({})];
 
 export const Dark = Template.bind({});
 Dark.args = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({
-    login: { username: 'efwef', password: '123' }
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
+
+export const DarkAuth = Template.bind({});
+DarkAuth.args = {};
+DarkAuth.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({
+    user: { authUser: {} }
 })];
