bcf87c2272918774c6a2e12c7968e4e351eff694
Vladyslav_Istomin <vladyslav_istomin@epam.com>
2023-02-25 17:07:09 +0200

    updated Navbar ui tests

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -3,6 +3,8 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Navbar } from './Navbar';
 import { Theme } from 'app/providers/ThemeProvider';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator';
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator';
+import { LoginElement } from 'features/AuthByUsername/ui/LoginForm/LoginForm.stories';
 
 export default {
     title: 'widgets/Navbar',
@@ -16,7 +18,12 @@ const Template: ComponentStory<typeof Navbar> = (args) => <Navbar {...args} />;
 
 export const Light = Template.bind({});
 Light.args = {};
+Light.decorators = [StoreDecorator({
+    login: { username: 'efwef', password: '123' }
+})];
 
 export const Dark = Template.bind({});
 Dark.args = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK)];
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({
+    login: { username: 'efwef', password: '123' }
+})];
