3d87ef0656260c81493cad5e1237a683b5e2c0b1
Vladyslav_Istomin <vladyslav_istomin@epam.com>
2023-02-26 19:23:40 +0200

    off ui tests

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -1,15 +1,15 @@
 module.exports = {
-    'stories': [
+    stories: [
         '../../src/**/*.stories.@(js|jsx|ts|tsx)'
     ],
-    'staticDirs': ['../../public'],
-    'addons': [
+    staticDirs: ['../../public'],
+    addons: [
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@storybook/addon-interactions'
     ],
-    'framework': '@storybook/react',
-    'core': {
-        'builder': '@storybook/builder-webpack5'
+    framework: '@storybook/react',
+    core: {
+        'builder': 'webpack5'
     }
 };
